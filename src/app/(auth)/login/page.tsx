1791538f243ba71efc2f54f98c3de534b427764a
TIEZA\tienn <tiennq201@gmail.com>
2025-06-22 20:55:36 +0700

    feat(home): ux services hint in home page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -6,7 +6,7 @@ import { FormForget } from "./components/FormForget";
 export default function Login() {
     const [choiseForgetPass, setChoiseForgetPass] = useState(false)
     return (
-        <div className="h-full flex justify-center items-center py-20 max-2xl:py-0">
+        <div className="h-full flex justify-center items-center py-20 max-[1540px]:py-0">
             {!choiseForgetPass ? 
             <FormLogin setChoiseForgetPass={setChoiseForgetPass} />:
             <FormForget setChoiseForgetPass={setChoiseForgetPass}/>
